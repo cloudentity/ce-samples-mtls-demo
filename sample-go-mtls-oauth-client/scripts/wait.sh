cf8c623443e033b37521ebc93720f8c91eddd6d3
Billy <bbray@cloudentity.com>
2022-02-10 19:22:55 -0500

    added pyron to makefile

---
+++ sample-go-mtls-oauth-client/scripts/wait.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+pendingServices=0
+
+function checkServices() {
+    pendingServices=$(docker ps --format "{{.Status}}" | grep -E "starting|unhealthy" | wc -l)
+}
+
+checkServices
+
+while [ $pendingServices -gt 0 ]; do
+    echo "Waiting for $pendingServices services to become healthy..."
+    docker ps -a
+    sleep 1
+    checkServices
+done
+
+echo "All services are healthy"
+echo "Start the authorization flow here http://localhost:18888/login"
